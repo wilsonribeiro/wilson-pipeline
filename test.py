f35d190714650634d90c8f07491b80c51c96272b
Wilson Ribeiro <wilson.ribeiro2@gmail.com>
2020-10-15 19:21:50 -0300

    App Upload

--- test.py
+++ test.py
@@ -19,7 +19,7 @@ class Test(unittest.TestCase):
         result = self.app.get('/') 
 
         # verifica o retorno do conteudo da pagina
-        self.assertRegex(result.data.decode(), "Testes do Wilson-PipeLine")
+        self.assertRegex(result.data.decode(), "Sejam Bem Vindos à Wilson-Pipeline")
 
 
 if __name__ == "__main__":
