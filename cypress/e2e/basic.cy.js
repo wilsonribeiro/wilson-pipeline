bf92386d6c77c7bf2f2067876d00efe5beee9f98
Wilson Ribeiro <wilson.ribeiro2@gmail.com>
2023-04-14 18:33:27 -0300

    Upload Files

--- cypress/e2e/basic.cy.js
+++ cypress/e2e/basic.cy.js
@@ -5,7 +5,7 @@ describe('sample test', () => {
 
   it('displays the resources text', () => {
     cy.get('h1')
-    .contains('Welcome to my app!');
+    .contains('Welcome to my Wilson-Pipeline App!');
   })
   it('renders the Netlify logo image', () => {
     cy.get('img')
