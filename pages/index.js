478eb7d49bc4c2788a1c90d56e1377ae743dce31
Wilson Ribeiro <wilson.ribeiro2@gmail.com>
2023-04-14 17:50:58 -0300

    Upload Files

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,13 @@ export default function Home() {
       <main>
         <Header title="Welcome to my Wilson-Pipeline App!" />
         <p className="description">
-          Application created to exemplify the cycle of a DevOps PipeLine.
+          Application created to exemplify the cycle of a DevOps PipeLine
+
+          For more information, visit my profile on [LinkedIn](https://www.linkedin.com/in/wilsonribeiro2/)
+
+          Thanks in advance!!
+
+          Wilson Ribeiro
         </p>
       </main>
 
